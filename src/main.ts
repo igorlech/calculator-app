662ba1a4cf4b9018ca831d80b6e0c17d1403cfb3
igorlech <eaaigle@students.eaaa.dk>
2023-06-21 12:57:58 +0200

    setup for operators logic

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,14 @@ import "./main.scss";
 
 const numberBtns = document.querySelectorAll<HTMLButtonElement>(".number");
 const numberBtnsArray = Array.from(numberBtns);
+const multiplyBtn = document.querySelector<HTMLButtonElement>("#multiply");
+const divideBtn = document.querySelector<HTMLButtonElement>("#divide");
+const subtractBtn = document.querySelector<HTMLButtonElement>("#subtract");
+const addBtn = document.querySelector<HTMLButtonElement>("#add");
+const equalsBtn = document.querySelector<HTMLButtonElement>("#equals");
 
 console.log(numberBtnsArray);
+console.log(multiplyBtn, divideBtn, subtractBtn, addBtn, equalsBtn);
 
 function Calculator() {
   numberBtnsArray.forEach((btn) => {
@@ -13,6 +19,26 @@ function Calculator() {
       console.log(+btn.value);
       console.log(typeof +btn.value);
     });
+  });
+
+  addBtn?.addEventListener("click", () => {
+    console.log("add");
+  });
+
+  subtractBtn?.addEventListener("click", () => {
+    console.log("subtract");
+  });
+
+  multiplyBtn?.addEventListener("click", () => {
+    console.log("multiply");
+  });
+
+  divideBtn?.addEventListener("click", () => {
+    console.log("divide");
+  });
+
+  equalsBtn?.addEventListener("click", () => {
+    console.log("equals");
   });
 }
 
