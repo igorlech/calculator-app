766e6d37d2b4326d651ab6f9601a8b3f62f84f6a
Igor Lech <igl-ext@illumi.dk>
2023-12-05 13:59:31 +0100

    add font styles and update calculator display

--- src/calculator/calculator.ts
+++ src/calculator/calculator.ts
@@ -12,7 +12,10 @@ const equalsBtn = document.querySelector<HTMLButtonElement>("#equals");
 
 const clearBtn = document.querySelector<HTMLButtonElement>("#clear");
 
-const currentNumber = document.querySelector<HTMLDivElement>("#current-number");
+const currentNumber = document.querySelector<HTMLDivElement>(".current-number");
+const previousNumber =
+  document.querySelector<HTMLDivElement>(".previous-number");
+const mathSign = document.querySelector<HTMLDivElement>(".math-sign");
 
 // Logic functions
 
@@ -39,16 +42,45 @@ function divide(a: number, b: number): number {
   return a / b;
 }
 
-// Main Calculator function
+// Operator function
 
-export default function Calculator() {
+function operate(a: number, b: number, operator: string): number {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      throw new Error("Invalid operator");
+  }
+}
+
+// Action functions
+
+function addMathSign(operator: string) {
+  mathSign?.appendChild(document.createTextNode(operator));
+}
+
+function getNumber() {
   numberBtnsArray.forEach((btn) => {
     btn.addEventListener("click", () => {
       console.log(+btn.value);
       console.log(typeof +btn.value);
+      if (currentNumber?.firstChild?.nodeValue === "0") {
+        currentNumber.removeChild(currentNumber.firstChild!);
+        currentNumber.appendChild(document.createTextNode(btn.value));
+      } else {
+        currentNumber?.appendChild(document.createTextNode(btn.value));
+      }
     });
   });
+}
 
+function arithmetic() {
   addBtn?.addEventListener("click", () => {
     console.log("add");
     add(1, 2);
@@ -72,4 +104,27 @@ export default function Calculator() {
   equalsBtn?.addEventListener("click", () => {
     console.log("equals");
   });
+}
+
+function clear() {
+  clearBtn?.addEventListener("click", () => {
+    console.log("clear");
+    if (mathSign?.firstChild) {
+      mathSign.removeChild(mathSign.firstChild!);
+    }
+
+    if (previousNumber?.firstChild) {
+      previousNumber.removeChild(previousNumber.firstChild!);
+    }
+
+    currentNumber?.replaceChildren(document.createTextNode("0"));
+  });
+}
+
+// Main Calculator function
+
+export default function Calculator() {
+  getNumber();
+  arithmetic();
+  clear();
 }
